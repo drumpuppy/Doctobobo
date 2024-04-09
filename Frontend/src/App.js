71f794101810451f105652c91f516ff4710dcaa6
LouiseESME <louise.martin.ndv@free.fr>
2024-04-09 17:46:44 +0200

    changement nom app

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,6 +4,7 @@ import Signup from "./pages/Signup";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeRoutes } from "./routes";
 function App() {
+  document.title = "Doctobobo";
   return (
     <Router>
       <ThemeRoutes />
