ca236ff6f46b0888830c8f8903b6ea7afb5b9efe
hanks92 <alexandre.rodgold@gmail.com>
2024-04-25 15:50:27 +0200

    changement moteur de recherche style 2.0

--- Frontend/src/Components/Search.jsx
+++ Frontend/src/Components/Search.jsx
@@ -68,40 +68,40 @@ const style = {
   textField: {
     '& .MuiOutlinedInput-root': {
       '& fieldset': {
-        borderColor: 'white', // Changing the border color
-        borderRadius: '15px', // Border radius for rounded corners
+        borderColor: 'white', 
+        borderRadius: '15px', 
         borderWidth: '5px',
       },
-      color: 'white', // Text color
+      color: 'white', 
     },
     '& .MuiInputLabel-root': {
-      color: 'white', // Label color
+      color: 'white', 
     },
     '& .MuiInputLabel-root.Mui-focused': {
-      color: 'white', // Label color when focused
+      color: 'white', 
     },
     '& .MuiInput-underline:before': {
-      borderBottomColor: 'white', // Underline color before focus
+      borderBottomColor: 'white',
     },
     '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
-      borderBottomColor: 'white', // Underline color on hover
+      borderBottomColor: 'white', 
     },
     '& .MuiInput-underline:after': {
-      borderBottomColor: 'white', // Underline color after focus
+      borderBottomColor: 'white',
     },
     '& .MuiOutlinedInput-input': {
-      color: 'white', // Input text color
+      color: 'white',
     },
     '& .MuiPlaceholder-root': {
-      color: 'white', // Placeholder text color
+      color: 'white',
     },
     '& .MuiInputBase-input::placeholder': {
-      color: 'white', // Placeholder text color
+      color: 'white', 
       opacity: 1,
     },
   },
   iconButton: {
-    color: 'white', // Icon button color
+    color: 'white',
   },
-  // ... (Other style definitions)
+
 };
